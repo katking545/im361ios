7465e855dd73a9d9fffb56ed009b299a1778e7fa
Katrina King <kmking@mail.bradley.edu>
2019-04-09 20:12:50 -0500

    Initial Commit

---
+++ PracticePro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticePro
+//
+//  Created by Katrina King on 4/9/19.
+//  Copyright © 2019 Katrina King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
