e14a77ed5f4abd57d1f3aefa2cfdce9f9b8989b9
Katrina King <kmking@mail.bradley.edu>
2019-04-23 14:48:41 -0500

    commit

--- PracticePro/ViewController.swift
+++ PracticePro/ViewController.swift
@@ -38,40 +38,9 @@ class ViewController: UIViewController {
             }
         }
         
-    @IBAction func didPressSwitch(_ sender: Any) {
-        isSegueAllowed = !isSegueAllowed
-    }
-    
-    @IBAction func didShowAlert(_ sender: Any) {
-        
-        let alert = UIAlertController(title: "Hello world!", message: "This is an alert", preferredStyle: .actionSheet)
     
     
-    let action1 = UIAlertAction(title: "Hello",
-                                style: .default) { _ in
-        print("Hello was clicked")
-    }
-    
-    let action2 = UIAlertAction(title: "Cancel",
-                                style:.cancel) { _ in
-        print("action was cancelled")
-    }
-    
-    let action3 = UIAlertAction(title: "DELETE",
-                                style: .destructive) { _ in
-        print("DELETE WAS CLICKED")
-    }
-    
-    alert.addAction(action1)
-    alert.addAction(action2)
-    alert.addAction(action3)
-        
-    present(alert,
-            animated: true)
-        print("alert was shown")
-        
-        
-    }
+   
     
 
 
